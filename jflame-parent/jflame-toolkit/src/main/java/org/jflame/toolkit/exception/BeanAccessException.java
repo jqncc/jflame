c68a27d05b7d95776a536733c2c3190b957330a1
yucan.zhang <yucan.zhang@XUCHENG-PC.gloryharvestgroup.com>
2017-03-28 17:35:04 +0800

    bean helper

---
+++ jflame-parent/jflame-toolkit/src/main/java/org/jflame/toolkit/exception/BeanAccessException.java
@@ -0,0 +1,27 @@
+package org.jflame.toolkit.exception;
+
+/**
+ * javabean操作异常类,如果bean的内省,反射读取或设置bean属性等操作出现异常.
+ * 
+ * @author zyc
+ */
+public class BeanAccessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BeanAccessException() {
+        super();
+    }
+
+    public BeanAccessException(String message) {
+        super(message);
+    }
+
+    public BeanAccessException(Throwable exception) {
+        super(exception);
+    }
+
+    public BeanAccessException(String message, Throwable exception) {
+        super(message, exception);
+    }
+}
